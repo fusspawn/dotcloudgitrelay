d8333d4a5a16b358515ad2c1d235b6a82066b9f8
fusspawn <ariochofmelnibone@gmail.com>
2012-01-21 01:05:31 +0000

    parsing json payload

--- watcher.js
+++ watcher.js
@@ -3,9 +3,16 @@ var exec = require('child_process').exec;
 var express = require('express');
 
 var app = express.createServer();
+app.use(express.bodyParser());
+
+
 
 app.post('/git/push', function(req, res){
-    console.log("git push recevied");
+    var payload = req.payload;
+    var git_data = JSON.parse(payload);
+    
+    console.log("got a git push for repo: " + git_data.repository.name + " \r\n " 
+                + "  message: " + git_data.commits[0].message);
 });
 
-app.listen(8081); //gi
+app.listen(8081);
