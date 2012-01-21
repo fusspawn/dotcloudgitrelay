0bba0eb53482714bf481c321db5135d9cdc20d9b
fusspawn <ariochofmelnibone@gmail.com>
2012-01-21 01:26:15 +0000

    added deps

--- watcher.js
+++ watcher.js
@@ -1,10 +1,12 @@
 var sys = require('util')
 var exec = require('child_process').exec;
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer();
 app.use(express.bodyParser());
 
+var known_gits = [];
 
 
 app.post('/git/push', function(req, res){
@@ -14,7 +16,49 @@ app.post('/git/push', function(req, res){
     var git_data = JSON.parse(payload);
     
     console.log("got a git push for repo: " + git_data.repository.name + " \r\n " 
-                + "  message: " + git_data.commits[git_data.commits.length - 1].message);
+                + "message: " + git_data.commits[git_data.commits.length - 1].message);
+    
+    try {
+        var stats = fs.lstatSync(git_data.repository.name);
+        if(stats.isDirectory()) {
+            git_pull(git_data);
+        }
+    } catch (exception) {
+            git_clone(git_data);    
+    } 
 });
+
+app.get("/", function(req, res) {
+        
+});
+
+function git_pull(git_data) {}
+
+function git_clone(git_data) {
+    var repo = git_data.repository.name;
+    var git_url = git_data.repository.url.replace("http://", "git://");
+    console.log("cloning: " + git_url);
+    known_gits.push(repo);
+    
+    var git_process = exec("git clone " + git_url);
+    git_process.stdout.on('data', function (data) {
+        if(data == "Password:")
+            git_process.stdin.write("trasher");
+    });
+    
+    git_process.stderr.on('data', function (data) {
+        console.log("Git (" + repo +") - Error: " + data);    
+    });
+    
+    git_process.on('exit', function(code) {
+        if (code !== 0) {
+            console.log("Git (" + repo +") - Error: Exit Code Not 0 was:" + code);
+        }
+        
+        console.log("Git (" + repo +") clone completed.");
+        git_process.stdin.end();
+    });
+}
+
 
 app.listen(8081);
