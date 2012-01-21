d7edf807206fff60e2e1e8d7948d6dfb1ae629e1
fusspawn <ariochofmelnibone@gmail.com>
2012-01-21 00:54:25 +0000

    added deps

--- watcher.js
+++ watcher.js
@@ -1,3 +1,13 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 var child;
+
+var logs = [];
+
+var app = express.createServer();
+
+app.get('/git/push', function(req, res){
+    console.log("git push recevied");
+});
+
+app.listen(8081);
