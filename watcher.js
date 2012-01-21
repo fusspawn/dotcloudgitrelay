43778bc30066f5dd87e213b25b05fb1235adff4e
fusspawn <ariochofmelnibone@gmail.com>
2012-01-21 01:00:55 +0000

    added deps

--- watcher.js
+++ watcher.js
@@ -4,8 +4,8 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.get('/git/push', function(req, res){
+app.post('/git/push', function(req, res){
     console.log("git push recevied");
 });
 
-app.listen(8081); //
+app.listen(8081); //gi
